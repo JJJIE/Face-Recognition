6a753e46b7aa06480e7beca377c0dbe4ab6c635b
JIN JIAJIE <JJJinGinger@gmail.com>
2021-03-13 23:29:55 +0900

    Initial version

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+from tqdm import tqdm
+
+
+def face_detect(img, cascade_name):
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.equalizeHist(img_gray)
+    face_cascade = cv2.CascadeClassifier(cascade_name)
+    faces = face_cascade.detectMultiScale(img)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 5)
+    return img
+
+
+def video_detect(file_name, cascade_name):
+
+    video = cv2.VideoCapture(file_name)  # 加载视频
+    fps = video.get(cv2.CAP_PROP_FPS)  # 帧率
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))  # 宽
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 高
+    fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')  # 指定视频编码方式
+    video_writer = cv2.VideoWriter('result.mp4', 0x7634706d, fps, (w, h))  # 创建视频写对象
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))  # 视频总帧数
+    for i in tqdm(range(frame_count)):  # 帧数遍历
+        success, img = video.read()  # 读取视频帧
+        img = face_detect(img, cascade_name)  # 帧检测
+        video_writer.write(img)  # 视频对象写入
+
+
+if __name__ == "__main__":
+    video_detect('testanime.mp4', 'lbpcascade_animeface.xml')
+    video_detect('testcat.mp4', 'haarcascade_frontalcatface.xml')
+
